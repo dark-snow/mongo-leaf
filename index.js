22cb0bc9e18917bb9a66187a37e6f6c7092bd624
Khaled Romdhane <heiyukidev@gmail.com>
2017-07-22 21:07:19 +0100

    exporting ObjectID

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const connect = require('./modules/db.module.js').connect;
 const Model = require('./modules/model.module.js');
-
+const ObjectID = require('mongodb').ObjectID;
 
 module.exports.connect = connect;
 module.exports.Model = Model;
+module.exports.ObjectID = ObjectID;
